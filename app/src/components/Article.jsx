3220a42399f9c3a48bbb900fd5007161a3937144
Алексей Зырянов <lexgp@yandex.ru>
2020-06-03 18:59:01 +0300

    05 Интерактивность, состояние компонентов

--- app/src/components/Article.jsx
+++ app/src/components/Article.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-  const {article} = props
-  const body = <section>{article.text}</section>
-  
-  return (
-    <div className='hello' style={{color: 'grey'}}>
-      <h2>{article.title}</h2>
-      {body}
-      <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-    </div>
-  )
-  
+class Article extends Component {
+  // constructor() {
+  //   super(props)
+  //   this.state = {
+  //     isOpen: true
+  //   }
+  // }
+  state = {
+    isOpen: true
+  }
+  render() {
+    const {article} = this.props
+    const body = this.state.isOpen && <section>{article.text}</section>
+    
+    return (
+      <div className='hello' style={{color: 'grey'}}>
+        <h2>
+          {article.title}
+          <button onClick={this.handleClick}>
+            {this.state.isOpen ? 'close' : 'open'}
+          </button>
+        </h2>
+        {body}
+        <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+      </div>
+    )  
+  }
+
+  handleClick = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+}
+
+function handleClick() {
+
 }
 
 export default Article
